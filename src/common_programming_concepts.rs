24afb7593ea6a55c194bd6500fc43083945bc848
imgbit <lukasz.hunczak@imaginarybit.com>
2023-04-19 20:04:44 +0200

    Adding fibonacci function, code formatting

--- src/common_programming_concepts.rs
+++ src/common_programming_concepts.rs
@@ -1,5 +1,16 @@
-pub fn fahrenheit_to_celsius(f : f64) -> f64 {
-    (f - 32.0) * 5.0/9.0
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+pub fn nth_fibonacci(f: u32) -> u32 {
+    if f == 0 {
+        0
+    } else if f == 1 {
+        1
+    } else {
+        // use recursion
+        nth_fibonacci(f - 1) + nth_fibonacci(f - 2)
+    }
 }
 
 #[cfg(test)]
@@ -12,5 +23,13 @@ mod tests {
         assert_eq!(fahrenheit_to_celsius(68.0), 20.0);
         assert_eq!(fahrenheit_to_celsius(212.0), 100.0);
         assert_eq!(fahrenheit_to_celsius(-1000.0), -573.3333333333334);
+    }
+
+    #[test]
+    fn test_nth_fibonacci() {
+        assert_eq!(nth_fibonacci(0), 0);
+        assert_eq!(nth_fibonacci(1), 1);
+        assert_eq!(nth_fibonacci(5), 5);
+        assert_eq!(nth_fibonacci(10), 55);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod common_programming_concepts;
 
 fn main() {
-    println!("common_programming_concepts::fahrenheit_to_celsius(100) = {}", common_programming_concepts::fahrenheit_to_celsius(100.0));
+    println!(
+        "common_programming_concepts::fahrenheit_to_celsius(100) = {}",
+        common_programming_concepts::fahrenheit_to_celsius(100.0)
+    );
+
+    println!(
+        "common_programming_concepts::nth_fibonacci(5) = {}",
+        common_programming_concepts::nth_fibonacci(10)
+    );
 }
