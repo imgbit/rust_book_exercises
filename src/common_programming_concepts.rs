c3eb53bb7d051a6ff6a924314f049b4fd4c68f91
imgbit <lukasz.hunczak@imaginarybit.com>
2023-04-20 21:47:06 +0200

    Adding third exercise from common programming concepts chapter

--- src/common_programming_concepts.rs
+++ src/common_programming_concepts.rs
@@ -13,6 +13,50 @@ pub fn nth_fibonacci(f: u32) -> u32 {
     }
 }
 
+pub fn write_a_christmas_carol() -> String {
+    let ordinals = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    let verses = [
+        "A partridge in a pear tree.",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    let mut acc = String::from(verses[0]);
+    let mut ret = String::new();
+    for (index, verse) in verses.iter().enumerate() {
+        if index != 0 {
+            acc = format!("{},\n{}", verse, acc);
+        }
+        ret = format!(
+            "{}\n{}\n{}\n",
+            ret,
+            format!(
+                "On the {} day of Christmas, my true love gave to me",
+                ordinals[index]
+            ),
+            acc
+        );
+        if index == 0 {
+            acc = format!("And {}", acc.to_lowercase());
+        }
+    }
+
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +75,113 @@ mod tests {
         assert_eq!(nth_fibonacci(1), 1);
         assert_eq!(nth_fibonacci(5), 5);
         assert_eq!(nth_fibonacci(10), 55);
+    }
+
+    #[test]
+    fn test_write_a_christmas_carol() {
+        let lyrics = r#"
+On the first day of Christmas, my true love gave to me
+A partridge in a pear tree.
+
+On the second day of Christmas, my true love gave to me
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the third day of Christmas, my true love gave to me
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the fourth day of Christmas, my true love gave to me
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the fifth day of Christmas, my true love gave to me
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the sixth day of Christmas, my true love gave to me
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the seventh day of Christmas, my true love gave to me
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the eighth day of Christmas, my true love gave to me
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the ninth day of Christmas, my true love gave to me
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the tenth day of Christmas, my true love gave to me
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the eleventh day of Christmas, my true love gave to me
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+
+On the twelfth day of Christmas, my true love gave to me
+Twelve drummers drumming,
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five golden rings,
+Four calling birds,
+Three French hens,
+Two turtle doves,
+And a partridge in a pear tree.
+"#;
+        assert_eq!(write_a_christmas_carol(), lyrics.to_string());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ mod common_programming_concepts;
 fn main() {
     println!(
         "common_programming_concepts::fahrenheit_to_celsius(100) = {}",
-        common_programming_concepts::fahrenheit_to_celsius(100.0)
+        common_programming_concepts::fahrenheit_to_celsius(100.0) as u32
     );
 
     println!(
         "common_programming_concepts::nth_fibonacci(5) = {}",
         common_programming_concepts::nth_fibonacci(10)
+    );
+
+    println!(
+        "common_programming_concepts::write_a_christmas_carol() = \"{}\"",
+        common_programming_concepts::write_a_christmas_carol()
     );
 }
